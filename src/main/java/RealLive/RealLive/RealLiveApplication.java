105da0af807ac2af855155fdf15b66a112102641
Lau Zhan You <119853913+donkey-king-kong@users.noreply.github.com>
2024-12-13 22:43:08 +0800

    Initializer SpringBoot with dependency

---
+++ src/main/java/RealLive/RealLive/RealLiveApplication.java
@@ -0,0 +1,13 @@
+package RealLive.RealLive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RealLive/RealLive/RealLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package RealLive.RealLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
