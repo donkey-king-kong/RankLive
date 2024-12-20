bc1836b8eb75820e7f33d464e6f9c4b6bb8aa057
Lau Zhan You <119853913+donkey-king-kong@users.noreply.github.com>
2024-12-20 23:14:57 +0800

    Created UML Diagram & models

---
+++ src/main/java/models/Leaderboard.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Leaderboard {
+    private String name;
+    private int size;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getSize(){
+        return size;
+    }
+}
---
+++ src/main/java/models/Player.java
@@ -0,0 +1,31 @@
+package models;
+
+public class Player {
+    private String id;
+    private String userName;
+    private int points;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
